2a8eba48103f281d7a39df815d2aa19f37635dd6
KIMDONGGYUN <nabilera1@naver.com>
2019-09-15 22:55:44 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace 구구단
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("구구단");
+            int i, j;
+            for(i=1;i<=9;i++)
+            {
+                for(j=1;j<=9;j++)
+                {
+                    Console.WriteLine("{0}*{1}={2}", i, j, i * j);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
